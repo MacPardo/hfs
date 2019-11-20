968ca13d95c5e61a07f9b09e08a729fc21f418b3
Marcos Theophilo Gobbi Adamczuk <marcos.adamczuk@gmail.com>
2019-11-20 01:07:38 -0300

    mudei a aparência do quizresults

--- src/components/quiz/QuizResults.vue
+++ src/components/quiz/QuizResults.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
+  <!-- <div>
     <p>Você acertou {{ correctCount }} de {{ questionCount }} </p>
     <ul>
       <li v-for="(answer, index) in answers" :key="index">{{ answer }}</li>
     </ul>
+  </div>-->
+  <div class="card mt-4 mb-4">
+    <div class="card-body">
+      <h3>
+        Você acertou
+        <span class="font-weight-bolder text-success">{{ correctCount }}</span> de
+        <span class="font-weight-bolder text-primary">{{ questionCount }}</span>
+      </h3>
+    </div>
   </div>
 </template>
 
