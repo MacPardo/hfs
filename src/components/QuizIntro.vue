04d14b8d7c04b18ec91462f874d79c9a07339b1c
Marcos Theophilo Gobbi Adamczuk <marcos.adamczuk@gmail.com>
2019-11-27 09:17:38 -0300

    arrumei erro de digitacao

--- src/components/QuizIntro.vue
+++ src/components/QuizIntro.vue
@@ -2,7 +2,7 @@
   <div class="container mission-section">
     <div class="row">
       <div class="col-lg-12">
-        <h1>Questinário</h1>
+        <h1>Questionário</h1>
         <p>Faça o questionário abaixo para testar seus conhecimentos!</p>
       </div>
     </div>
