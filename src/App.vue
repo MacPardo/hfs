9016a43fd61c22224f2d0268217d81c4c1582509
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2019-11-27 01:28:37 -0300

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,122 @@
 <template>
-  <div id="app">
-    <div class="jumbotron mission-header">
-      <h1 class="text-light text-xl-center">JESUÍTAS E OS SETE POVOS DAS MISSÕES</h1>
-    </div>
-    <div class="container">
-      <div class="text-left">
+  <div id="app" class="container">
+    <h1>JESUÍTAS E OS SETE POVOS DAS MISSÕES</h1>
 
-        <Origem />
-        <OrganizacaoUrbanistica />
-        <OrganizacaoEconomica />
-        <GuerraGuaranitica />
-        <QuizIntro />
+    <div class="text-left">
+      
+      <hr />
+      <h1>Origem</h1>
+      <hr />
+      <p>Durante a colonização do Brasil pelos portugueses, a ordem Companhia de Jesus, 
+        conhecidos como Jesuítas, cumpriu um papel de destaque na organização social da colônia.
+        Os jesuítas liderados por Manoel da Nóbrega chegaram à colônia Brasil em 1549, junto 
+        a Tomé de Sousa, que foi o primeiro governador-geral enviado por Portugal. A principal 
+        função dos jesuítas, ao virem ao Brasil, era <b>evangelizar</b>, <b>catequizar</b> e tornar cristãos os 
+        indígenas que habitavam estas terras.</p>
+      <p>Para que a catequização fosse realizada, os jesuítas aprenderam o Tupi,
+        língua utilizada pelos indígenas Guarani, assim como ensinaram os indígenas a
+        língua portuguesa para a leitura de trechos bíblicos e o ensino da prática religiosa
+        católica. Um dos nomes mais conhecidos no processo de evangelização que
+      chegaram até nós foi o do padre José de Anchieta.</p>
+      <p>Como a educação tradicional indígena era diferente, baseada na solidariedade e na 
+        cooperação, os indígenas mais novos aprendiam com os mais velhos, foram necessárias 
+        algumas mudanças.</p>
+      <p>Nas <b>missões</b>, locais onde os jesuítas habitavam por vezes com milhares de 
+        indígenas, construiu-se um processo de trocas culturais onde muitas coisas foram
+        adaptadas ao modo de viver indígena. De certo modo havia também o interesse que
+        os indígenas passassem a viver de acordo com a cultura europeia: que as famílias 
+        fossem nucleares 
+        (pai, mãe e filhos do casal), que eles se fixassem em um local (grande parte das tribos 
+        indígenas era semi nômade, vivendo em constante deslocamento e passassem a adotar os 
+        ritmos e as disciplinas de trabalho que impunham os europeus. Esse processo ficou também 
+        conhecido como aculturação.</p>
 
-      </div>
-      <Quiz />
-    </div>
+      <hr />
+      <h1>Sete Povos das Missões</h1>
+      <hr />
+      <p>Em 1682, os jesuítas e os indígenas, começaram a retornar à margem esquerda do rio 
+        Uruguai, às terras do atual Rio Grande do Sul, fundando os <b>Sete Povos das Missões</b>. 
+        Foram eles:</p>
+      <p>● São Francisco de Borja (1682); - pelo Padre Francisco Garcia</p>
+      <p>● São Nicolau (1687);</p>
+      <p>● São Luiz Gonzaga (1687);</p>
+      <p>● São Miguel Arcanjo (1687) foi a capital;</p>
+      <p>● São Lourenço Mártir (1690);</p>
+      <p>● São João Batista (1697); foi fundada pelo músico e arquiteto Padre Antônio Sepp, 
+      que por ter sólida instrução de música vocal e instrumental, o que lhe deu grande 
+      destaque como músico. Os instrumentos de sua orquestra foram feitos por ele e seus 
+      discípulos. Extraiu o primeiro ferro das Missões, fazendo instrumentos de toda espécie, 
+      bem como os sinos do seu povo. Sua obra-prima foi o relógio da torre da igreja que, ao 
+      dar as horas, fazia desfilar, pelo mostrador, as imagens dos doze apóstolos.</p>
+      <p>● Santo  Ângelo Custódio (1706). Antes de fundarem cada aldeia, os jesuítas cuidavam 
+      para que a escolha recaísse em lugares altos, de fácil defesa, com matas e água 
+      abundante. Com alguns indígenas, iniciavam as plantações e as construções provisórias. 
+      Quando as lavouras já estavam produzindo, vinham as famílias, que começavam a erguer 
+      as casas projetadas pelos padres. As povoações cresciam em quarteirões regulares, 
+      conforme a arquitetura e o urbanismo típicos dos espanhóis, e idealizado pelo Padre 
+      Roque Gonzáles, muito antes de morrer.</p>
+      <p>Com o trabalho coletivo dos indígenas e a coordenação geral dos jesuítas, foram construídos 
+        os "30 Povos das Missões", em áreas que hoje fazem parte do Brasil, Argentina e Paraguai.</p>
+
+      <hr />
+      <h1>Organização Urbanística</h1>
+      <hr />
+      <p>Nessa época, a praça era considerada como centro e a igreja como o prédio mais 
+        importante da região. Junto à igreja ficava a residência dos padres, o colégio, as 
+        oficinas, o cemitério. As casas dos indígenas, contornavam a praça.</p>
+      <p>Num dos lados da igreja estava o cemitério e a casa grande, chamada de  COTIAGUAÇU, 
+        onde abrigavam-se as viúvas, as mulheres sozinhas e os órfãos.</p>
+      <p>No outro lado da igreja ficava o colégio, onde era ministrado o ensino. Na enfermaria, 
+        localizada ao lado do cemitério, atendiam-se os doentes</p>
+      <p>Os indígenas, eram enterrados no cemitério, os jesuítas ficavam enterrados na igreja, 
+        junto ao alto mor. Atrás da igreja, os padres mantinham a horta, o pomar e o jardim.</p>
+
+      <hr />
+      <h1>Organização Econômica</h1>
+      <hr />
+      <p>Nos locais onde havia água, a população construía fontes de pedras para se abastecer, lavar a roupa e tomar banho.</p>
+      <p>A agricultura era o principal meio de  prover à alimentação dos indígenas,. Eles cultivavam milho, mandioca, abóbora, algodão, cana-de-açúcar, hortaliças e árvores frutíferas. Os excedentes eram comercializados..</p>
+      <p>A criação de animais fazia-se em estâncias longe dos povos. Os indígenas, também se serviam do leite, couro e sebo.</p>
+      <p>Música, canto, dança, teatro, desenho, pintura e escultura foram os recursos usados pelos jesuítas, como apoio à catequese. Desde pequenos, alguns indígenas, aprendiam a tecer e a fabricar instrumentos musicais com cópias originais europeias.</p>
+      <p>Nos Sete Povos das Missões, extraía-se erva-mate, ferro e madeira. Logo, os indígenas, aprenderam a fazer mudas e plantaram grandes ervais.</p>
+      <p>Os indígenas, Guaranis das Missões vestiam-se conforme a orientação dos padres jesuítas. As roupas de uso comum eram feitas pelas donas de casa. Umas faziam o fio, outras teciam.</p>
+      <p>As missões serviam como áreas protegidas da ação dos colonos, que buscavam escravizar os indígenas.</p>
+      <p>Além do que era comercializado nas missões, os jesuítas conseguiram acumular fortuna com a posse de enormes extensões de terras e engenhos.</p>
+
+      <hr />
+      <h1>Guerra Guaranítica</h1>
+      <hr />
+      <p>Em 1750, Portugal e Espanha assinaram o Tratado de Madri. Esse tratado redefinia a divisão das terras da América do Sul entre portugueses e espanhóis. De acordo com ele, a região dos Sete Povos das Missões (atual região oeste do RS), que era da Espanha, deveria ser entregue aos portugueses.</p>
+      <p>Os jesuítas espanhóis, que atuavam na área, não aceitaram o acordo e armaram os indígenas da região.</p>
+      <p>Quando os portugueses foram tomar posse do local, em 1754, houve conflito militar entre esses e os indígenas,, que não aceitaram deixar suas terras. Teve início então a Guerra Guaranítica.</p>
+      <p>O principal líder indígena guarani, durante a Guerra Guaranítica, foi Sepé Tiaraju. É considerado até hoje uma espécie de herói e símbolo da resistência indígena contra a opressão dos colonizadores europeus.</p>
+      <p>As principais consequências desta guerra foi a morte de mais de vinte mil indígenas, guaranis da região dos Sete Povos das Missões, a destruição dos Sete Povos das Missões em 1756 e a diminuição da influência dos jesuítas na região sul do Brasil.</p>
+  
+      <hr />
+      <h1>Quiz sobre o seu aprendizado</h1>
+      <hr /></div>
+    <Quiz />
   </div>
 </template>
 
 <script>
 import Quiz from "./components/quiz/Quiz.vue";
-import Origem from "./components/Origem";
-import OrganizacaoUrbanistica from "./components/OrganizacaoUrbanistica";
-import OrganizacaoEconomica from "./components/OrganizacaoEconomica";
-import GuerraGuaranitica from "./components/GuerraGuaranitica";
-import QuizIntro from "./components/QuizIntro";
 
 export default {
   name: "app",
   components: {
-    Quiz,
-    Origem,
-    OrganizacaoUrbanistica,
-    OrganizacaoEconomica,
-    GuerraGuaranitica,
-    QuizIntro
+    Quiz
   }
 };
 </script>
 
 <style>
-
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-.jumbotron.mission-header {
-  padding: 0;
-  background: url('assets/primeiramissasp.jpg') center center no-repeat;
-  background-size: cover;
-  position: relative;
-  height: 70vh;
-  width: 100%;
-}
-
-.jumbotron.mission-header::before {
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0.4, 0.4, 0.4, 0.4);
-  z-index: 0;
-}
-
-.jumbotron.mission-header h1 {
-  position: absolute;
-  z-index: 1;
-  width: 100%;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-
-.mission-section {
-  margin: 4rem 0;
+  margin-top: 60px;
 }
 </style>
